9374fbd869f262745afbff0bd866f6f6b062cb5d
Tom <tommarz@post.bgu.ac.il>
2020-01-20 21:59:00 +0200

    added data_extractor to main

--- server/src/main.py
+++ server/src/main.py
@@ -9,6 +9,7 @@ import numpy as np
 import visualizer
 import video_proccesor
 import data_analyser
+import data_extractor
 import utils
 
 # import preprocessor
@@ -71,13 +72,11 @@ def interpolate_and_plot(csv_path, y_cols, x_col='Frame Number', interp_csv_path
 def main():
     # for output dirs keys - see utils.generate_dirs_for_output_of_movie
     output_dirs = video_proccesor.get_keypoints_csv_from_video(args, params)
-    data_analyser.filter_anomalies(output_dirs)
-    data_analyser.make_interpolation(output_dirs)
-    data_analyser.make_body_parts_df(pd.read_csv(output_dirs['analytical_data_path'] + '/all_keypoints.csv'),
+    data_extractor.make_body_parts_df(pd.read_csv(output_dirs['analytical_data_path'] + '/all_keypoints.csv'),
                                      output_dirs)
     vectors = pd.read_csv(output_dirs['analytical_data_path'] + '/vectors_by_time.csv')
-    data_analyser.make_angle_df(vectors, output_dirs)
-    data_analyser.make_body_part_detected_by_frame_df(output_dirs)
+    data_extractor.make_angle_df(vectors, output_dirs)
+    data_extractor.make_body_part_detected_by_frame_df(output_dirs)
     visualizer.create_all_figures(output_dirs)
     utils.zip_output(output_dirs)
     utils.delete_generate_dirs(output_dirs)
