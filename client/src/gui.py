70db8fd896d5d8c7ac16666520a95d572310c52b
tommarz <tommarz@post.bgu.ac.il>
2020-03-28 18:25:13 +0300

    Created a TCP socket between client and server. Currently only MOV videos are working. TODO: check mp4 movies (not working)

--- client/src/gui.py
+++ client/src/gui.py
@@ -4,6 +4,7 @@ import preprocessor
 from shutil import copyfile
 from waitress import serve
 import shutil
+from socket import *
 
 # from werkzeug import secure_filename
 
@@ -18,23 +19,34 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'MOV', 'mp4
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+CHUNKSIZE = 1_000_000
+
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
-def send_file_to_server(video_path):
-    video_name = video_path.split('/')[-1]
-    video_name = video_name.split('.')[0]
-    # to create the output dir from the server
-    if not os.path.exists('output'):
-        print('Created output dir')
-        os.mkdir('output')
-    copyfile(video_path, "../../server/videos/" + video_name + ".mp4")
-    # os.remove(video_path)
-    shutil.rmtree('partial_movies')
-    print("Sent file!")
+def send_file_to_server(video_path, filename):
+    sock = socket()  # Create a socket object
+    host = '132.72.96.31'
+    port = 12345  # Reserve a port for your service.
+    sock.connect((host, port))
+
+    with sock, open(video_path, 'rb') as f:
+        sock.sendall(filename.encode() + b'\n')
+        sock.sendall(f'{os.path.getsize(video_path)}'.encode() + b'\n')
+        # Send the file in chunks so large files can be handled.
+        while True:
+            data = f.read(CHUNKSIZE)
+            if not data:
+                break
+            sock.sendall(data)
+        f.close()
+        print("Done Sending")
+        sock.shutdown(SHUT_WR)
+        print(sock.recv(1024).decode())
+        sock.close()
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -51,7 +63,8 @@ def upload_file():
             video_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(video_path)
             video_name = preprocessor.video_cutter(video_path)
-            send_file_to_server("partial_movies/" + video_name + ".mp4")
+            # send_file_to_server("partial_movies/" + video_name + ".mp4",  video_name + ".mp4")
+            send_file_to_server("uploaded_files/" + video_name + ".MOV", video_name + ".MOV")
             return redirect(url_for('upload_file'))
         else:
             return '''
--- server/src/main.py
+++ server/src/main.py
@@ -7,6 +7,9 @@ import argparse
 import facade
 import time
 import shutil
+from socket import *
+import threading
+from multiprocessing import Process
 
 # import preprocessor
 # setup
@@ -58,27 +61,91 @@ for i in range(0, len(args[1])):
         key = curr_item.replace('-', '')
         if key not in params: params[key] = next_item
 
+CHUNKSIZE = 1_000_000
+
+
+def wait_recv_video():
+    print("Receiver started..")
+    host = '132.72.96.31'
+    sock = socket()         # Create a socket object
+    port = 12345                 # Reserve a port for your service.
+    sock.bind((host, port))        # Bind to the port
+    sock.listen(5)                 # Now wait for client connection.
+
+    # while True:
+    #     c, addr = sock.accept()     # Establish connection with client.
+    #     print('Got connection from', addr)
+    #     filename_len = int(c.recv(4).decode())
+    #     # c.send('filename length is {}'.format(filename_len).encode())
+    #     print('filename length is {}'.format(filename_len))
+    #     filename = c.recv(filename_len).decode()
+    #     # c.send("Receiving filename: {}".format(filename).encode())
+    #     print("Receiving filename: {}".format(filename))
+    #     f = open('../videos/' + filename, 'wb')
+    #     # l = c.recv(1024)
+    #     print("Started receiving file...")
+    #     while True:
+    #         recvfile = c.recv(4096)
+    #         if not recvfile:
+    #             break
+    #         f.write(recvfile)
+    #     f.close()
+    #     print("Done Receiving")
+    #     c.send('Thank you for connecting'.encode())
+    #     c.close()
+
+    with sock:
+        while True:
+            client, addr = sock.accept()
+            # Use a socket.makefile() object to treat the socket as a file.
+            # Then, readline() can be used to read the newline-terminated metadata.
+            with client, client.makefile('rb') as clientfile:
+                filename = clientfile.readline().strip().decode()
+                length = int(clientfile.readline())
+                print(f'Downloading {filename}:{length}...')
+                path = os.path.join('../videos', filename)
+
+                # Read the data in chunks so it can handle large files.
+                with open(path, 'wb') as f:
+                    while length:
+                        chunk = min(length, CHUNKSIZE)
+                        data = clientfile.read(chunk)
+                        if not data:
+                            break # socket closed
+                        f.write(data)
+                        length -= len(data)
+
+                if length != 0:
+                    print('Invalid download.')
+                else:
+                    print('Done.')
+
 
 def wait_analyze_video():
+    print("Analyzer started..")
     while True:
-        for filename in os.listdir('../videos')[:1]:
-            print(filename)
-            video_path = '../videos/' + filename
-            print(video_path)
-            print("Analysing path...")
-            all_keypoints_csv_path = facade.get_keypoints_csv_from_video(video_path, params)
-            interpolated_keypoints_path = facade.interpolate_and_plot(all_keypoints_csv_path)
-            facade.get_angles_csv_from_keypoints_csv(interpolated_keypoints_path)
-            facade.get_detected_keypoints_by_frame(all_keypoints_csv_path)
-            facade.get_average_swimming_period_from_csv(interpolated_keypoints_path)
-            zip_path = facade.zip_output()
-            os.remove(video_path)
-            print("Removed video")
-            # else:
-            #     raise ValueError("%s isn't a file!" % filename)
-        print("Waiting...")
-        time.sleep(1)
+        try:
+            for filename in os.listdir('../videos')[:1]:
+                print(filename)
+                video_path = '../videos/' + filename
+                print("Analysing path: " + video_path)
+                all_keypoints_csv_path = facade.get_keypoints_csv_from_video(video_path, params)
+                interpolated_keypoints_path = facade.interpolate_and_plot(all_keypoints_csv_path)
+                facade.get_angles_csv_from_keypoints_csv(interpolated_keypoints_path)
+                facade.get_detected_keypoints_by_frame(all_keypoints_csv_path)
+                facade.get_average_swimming_period_from_csv(interpolated_keypoints_path)
+                zip_path = facade.zip_output()
+                os.remove(video_path)
+                print("Removed video")
+                # else:
+                #     raise ValueError("%s isn't a file!" % filename)
+            time.sleep(1)
+        except():
+            print("error")
 
 
 if __name__ == '__main__':
-    wait_analyze_video()
+    analyzer = Process(target=wait_analyze_video)
+    receiver = Process(target=wait_recv_video)
+    receiver.start()
+    # analyzer.start()
