578d1a410abdb913a9fe6114cb5a027ac17caa15
roeegro <roeegro@post.bgu.ac.il>
2020-05-02 14:21:39 +0300

    Fixed video trimmer
    The trimmer from the merge was not updated. Now it is updated

--- client/src/preprocessor.py
+++ client/src/preprocessor.py
@@ -2,21 +2,27 @@
 
 # import the necessary packages
 # import math
+import math
 
 from imutils.video import VideoStream
 import imutils
 import time
 import cv2
-from moviepy.editor import *
 import os
-from natsort import natsorted
-from moviepy.video.io.VideoFileClip import VideoFileClip
 
+from moviepy.config import get_setting
+from moviepy.tools import subprocess_call
 
-# from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+output_dir = "partial_movies/"
 
 
 # if the video_path argument is 0, then we are reading from webcam
+def get_video_name_from_path(video_path):
+    video_name = video_path.split('/')[-1]
+    video_name = video_name.split('\\')[-1]
+    video_name = video_name.split('.')[0]
+    return video_name
+
 
 def video_cutter(video_path=0):
     if video_path == 0:
@@ -32,12 +38,22 @@ def video_cutter(video_path=0):
     first_frame = imutils.resize(first_frame, width=500)
     first_frame = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
     first_frame = cv2.GaussianBlur(first_frame, (21, 21), 0)
-    min_area = 4000  # motion sensitivity factor
-    # initialize counter
-    # saving this variables in order to avoid duplicate checking of the same elapsed time
+
+    ####### PARAMETERS ######
+    MIN_AREA = 1600  # motion sensitivity factor
+    PIXEL_THRESH = 40
+    UNDETECTED_FRAMES_THRESH = 30
+    OMIT_CLIPS_BELOW = 2  # ignore videos with length < OMIT_CLIPS_BELOW seconds
+    DEBUG_MODE = 0  # visual feedback and prints
+    #########################
+
     is_recording = False
     frame_counter = 0
     num_frames_not_detected_in_seq = 0
+    prev_cnt = 0
+    prev_trend = 0
+    trend = 0  # to calculate if the swimmers is getting away from the camera
+    trend_diff = 0
     starting_frames = []
     starting_timestamps = []
     ending_timestamps = []
@@ -48,7 +64,7 @@ def video_cutter(video_path=0):
         frame = vs.read()
         fps = vs.get(5)
         frame = frame if video_path == 0 else frame[1]
-        if not frame is None:
+        if frame is not None:
             frame_shape = frame.shape
 
         # if the frame could not be grabbed, then we have reached the end
@@ -70,7 +86,7 @@ def video_cutter(video_path=0):
         # compute the absolute difference between the current frame and
         # first frame
         frame_delta = cv2.absdiff(first_frame, gray)
-        thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.threshold(frame_delta, PIXEL_THRESH, 255, cv2.THRESH_BINARY)[1]
 
         # dilate the thresholded image to fill in holes, then find contours
         # on thresholded image
@@ -78,78 +94,92 @@ def video_cutter(video_path=0):
         cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
                                 cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
-
-        found_countour_in_area = False
-        # loop over the contours
+        cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:1]  ## Taking the biggest countour
+        found_contour_in_area = False
+        # loop over the contours - in this implementation there is only the biggest contour
         for c in cnts:
             # if the contour is too small, ignore it
-            if cv2.contourArea(c) < min_area:
+            if DEBUG_MODE:
+                print("frame number: " + str(frame_counter) + " contourArea: " + str(
+                    cv2.contourArea(c)) + " trend: " + str(trend) + " trend diff: " + str(trend_diff))
+
+            if cv2.contourArea(c) < MIN_AREA:
+                trend = 0
                 continue
 
-            # (x, y, w, h) = cv2.boundingRect(c)
-            # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            # motion detected
-            found_countour_in_area = True
+            (x, y, w, h) = cv2.boundingRect(c)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            # if the current figure (aka contour) is bigger than the previous one - the swimmer is getting closer
+            trend = trend + 1 if (cv2.contourArea(c) - prev_cnt > 0) else trend - 1
+            prev_cnt = cv2.contourArea(c)
 
-            if not is_recording:  # otherwise - open new partial movie and write the first detected frame.
+            if not (frame_counter % int(fps)):
+                trend_diff = trend - prev_trend
+                prev_trend = trend
+            # motion detected
+            found_contour_in_area = True
+
+            # if there is movement towards the camera and record is off - start recording
+            if not is_recording and trend_diff >= 0:
                 starting_timestamps.append(frame_counter * 1.0 / fps)
                 starting_frames.append(frame_counter)
-                # fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
-                # partial_output = cv2.VideoWriter(
-                #     'partial_movies/partial_output_from_frame_{}.mp4'.format(frame_counter), fourcc, vs.get(5),
-                #     (frame.shape[1], frame.shape[0]))
-                # partial_output.write(frame)
-
-                # cv2.imshow('frame', frame)
                 is_recording = True
 
-        # update num of undetected frames in sequence if necessary
-        num_frames_not_detected_in_seq = 0 if found_countour_in_area else num_frames_not_detected_in_seq + 1
-        found_countour_in_area = False
+        if DEBUG_MODE:
+            cv2.imshow("Security Feed", frame)
+            cv2.imshow("Thresh", thresh)
+            cv2.imshow("Frame Delta", frame_delta)
+            key = cv2.waitKey(1) & 0xFF
 
-        # if there are more then 30 undetected frames in seq from the last frame detected - save the partial movies
-        if num_frames_not_detected_in_seq > 30 and not partial_output is None:
-            partial_output.release()
+        # update num of undetected frames in sequence if necessary
+        num_frames_not_detected_in_seq = 0 if found_contour_in_area else num_frames_not_detected_in_seq + 1
+
+        # if there are more then 30 undetected frames in seq from the last frame detected
+        # or the figure is getting away from the camera- end current recording
+        if (num_frames_not_detected_in_seq > UNDETECTED_FRAMES_THRESH or trend_diff < 0) and is_recording:
             ending_timestamps.append(frame_counter * 1.0 / fps)
-            partial_output = None
             is_recording = False
-            num_frames_not_detected_in_seq = 0
 
         frame_counter += 1
 
-    # Close all resources
-    if is_recording and not partial_output is None:
-        partial_output.release()
-
-    ending_timestamps.append(frame_counter * 1.0 / fps)
+    if is_recording:
+        ending_timestamps.append(frame_counter * 1.0 / fps)
     vs.stop() if video_path == 0 else vs.release()
     cv2.destroyAllWindows()
-
+    video_name = get_video_name_from_path(video_path)
+    new_videos_paths = []
     for start_frame, start_time, end_time in zip(starting_frames, starting_timestamps, ending_timestamps):
-        # ffmpeg_extract_subclip(video_path, start_time, end_time,
-        #                        targetname='partial_movies/partial_output_from_frame_{}.mp4'.format(start_frame))
-        with VideoFileClip(video_path) as video:
-            new = video.subclip(start_time, end_time)
-            new.write_videofile('partial_movies/partial_output_from_frame_{}.mp4'.format(start_frame),
-                                audio_codec='aac')
+        if DEBUG_MODE: print("Detected start time: " + str(start_time) + " Detected end time: " + str(end_time))
 
-    lst = []
+        if end_time - start_time < OMIT_CLIPS_BELOW:
+            if DEBUG_MODE: print("Omitting the clip")
+            continue
 
-    for root, dirs, files in os.walk('partial_movies/'):
-        files = natsorted(files)
-        # print(files)
-        for file in files:
-            if os.path.splitext(file)[1] == '.mp4':
-                file_path = os.path.join(root, file)
-                # print(file_path)
-                video = VideoFileClip(file_path)
-                # print(video)
-                lst.append(video)
+        new_start_time = math.floor(start_time) - 2
+        new_end_time = math.ceil(end_time)
+        start_frame = int(new_start_time * fps)
+        target_path = output_dir + video_name + '_from_frame_' + str(start_frame) + '.mp4'
 
-    # print(lst)
-    final_clip = concatenate_videoclips(lst)
-    video_name = video_path.split('\\')[-1]
-    video_name = video_name.split('.')[0]
-    final_clip.to_videofile("partial_movies/"+video_name+".mp4", fps=fps, remove_temp=False)
-    return video_name
+        if DEBUG_MODE:
+            print("Actually cutting between " + str(new_start_time) + " to " + str(new_end_time))
+            print(target_path)
+
+        extract_subclip(video_path, new_start_time, new_end_time, target_path)
+        new_videos_paths.append(target_path)
+
+    print(new_videos_paths)
+    return new_videos_paths
+
+
+def extract_subclip(filename, t1, t2, targetname):
+    """ Makes a new video file playing video file ``filename`` between
+        the times ``t1`` and ``t2``. """
+    name, ext = os.path.splitext(filename)
+    cmd = [get_setting("FFMPEG_BINARY"), "-y",
+           "-i", filename,
+           "-ss", "%.2f" % t1,
+           "-to", "%.2f" % t2,
+           "-vcodec", "copy", "-an", targetname]
+
+    subprocess_call(cmd)
