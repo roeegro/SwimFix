78374dbafdfa8cdafe6c050ebe999b619cd9fdc0
roeegro <roeegro@post.bgu.ac.il>
2020-05-15 14:11:25 +0300

    Update area-js to show mutilple lines in graph

--- client/src/static/js/demo/chart-area-demo.js
+++ client/src/static/js/demo/chart-area-demo.js
@@ -1,6 +1,7 @@
 // Set new default font family and font color to mimic Bootstrap's default styling
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
+var csv_name = null
 function number_format(number, decimals, dec_point, thousands_sep) {
     // *     example: number_format(1234.56, 2, ',', ' ');
     // *     return: '1 234,56'
@@ -28,32 +29,45 @@ function number_format(number, decimals, dec_point, thousands_sep) {
 
 function make_chart_from_csv(csv_path) {
     console.log("path is " + csv_path)
+    let splited_by_slash = csv_path.split('/')
+    let name_with_extension = splited_by_slash[splited_by_slash.length - 1]
+    csv_name = name_with_extension.split('.')[0]
     d3.csv(csv_path).then(make_chart)
     return 0
 }
 
 function make_chart(data) {
-    console.log(data)
     columns = d3.keys(data[0])
-    console.log(columns)
     columns.forEach(column => {
+        console.log(columns)
         var frame_range = []
         for (i = 0; i < data.length; i++) {
             var frame_num = (data[i])["Frame Number"]
             frame_range.push(frame_num)
         }
-        // console.log(frame_range)
-        var y_axis = []
-        for (i = 0; i < data.length; i++) {
-            var curr_data = (data[i])[column]
-            y_axis.push(curr_data)
+        // Get the match body part keypoint from the other side of the body (E.G if we have RShoulder, we will match it with LShoulder)
+        var column_prefix = column[0]
+        var body_part_name = column.slice(1)
+        var match_keypoint_column = body_part_name
+        var should_plot_multiple_graphs = false
+        var body_sides = []
+        body_sides.push('R')
+        body_sides.push('L')
+        console.log("current column = " + column)
+        if (body_sides.indexOf(column_prefix) != -1) {
+            match_keypoint_column = body_sides[1 + (body_sides.indexOf(column_prefix) % 2)] + body_part_name
+            should_plot_multiple_graphs = true
         }
-        // console.log(y_axis)
-        if (column == columns[0])
-            return;
+
         var charts_node = document.getElementById('Charts')
         var canvas_tag = document.createElement("canvas")
-        canvas_tag.setAttribute("id", column + "_graph")
+        console.log(body_part_name)
+        if (should_plot_multiple_graphs == true) {
+            canvas_tag.setAttribute("id", body_part_name + " graph")
+        }
+        else {
+            canvas_tag.setAttribute("id", column + " graph")
+        }
 
         var div_wrapper_for_canvas = document.createElement('div')
         div_wrapper_for_canvas.setAttribute('class', 'chart-bar')
@@ -65,7 +79,12 @@ function make_chart(data) {
 
         var h6_tag = document.createElement('h6')
         h6_tag.setAttribute('class', 'm-0 font-weight-bold text-primary')
-        h6_tag.innerText = column + ' Graph'
+        if (should_plot_multiple_graphs == true) {
+            h6_tag.innerText = body_part_name + ' Graph from csv : ' + csv_name
+        }
+        else{
+            h6_tag.innerText = column + ' Graph from csv : ' + csv_name
+        }
 
         var card_header_div_tag = document.createElement('div')
         card_header_div_tag.setAttribute('class', 'card-header py-3')
@@ -78,95 +97,212 @@ function make_chart(data) {
         card_shadow_div.appendChild(div_card_body)
 
         charts_node.appendChild(card_shadow_div)
-        // var ctx = document.getElementById('our_area_chart')
-        var myLineChart = new Chart(canvas_tag, {
-            type: 'line',
-            data: {
-                labels: frame_range,
-                datasets: [{
-                    label: "Value ",
-                    lineTension: 0.3,
-                    backgroundColor: "rgba(78, 115, 223, 0.05)",
-                    borderColor: "rgba(78, 115, 223, 1)",
-                    pointRadius: 3,
-                    pointBackgroundColor: "rgba(78, 115, 223, 1)",
-                    pointBorderColor: "rgba(78, 115, 223, 1)",
-                    pointHoverRadius: 3,
-                    pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
-                    pointHoverBorderColor: "rgba(78, 115, 223, 1)",
-                    pointHitRadius: 10,
-                    pointBorderWidth: 2,
-                    data: y_axis,
-                }],
-            },
-            options: {
-                maintainAspectRatio: false,
-                layout: {
-                    padding: {
-                        left: 10,
-                        right: 25,
-                        top: 25,
-                        bottom: 0
-                    }
-                },
-                scales: {
-                    xAxes: [{
-                        time: {
-                            unit: 'date'
-                        },
-                        gridLines: {
-                            display: false,
-                            drawBorder: false
-                        },
-                        ticks: {
-                            maxTicksLimit: 7
-                        }
+        if (column == columns[0])
+            return;
+
+        var y1_axis = []
+        for (i = 0; i < data.length; i++) {
+            var curr_data = (data[i])[column]
+            y1_axis.push(curr_data)
+        }
+
+        if (!should_plot_multiple_graphs) {
+            var myLineChart = new Chart(canvas_tag, {
+                type: 'line',
+                data: {
+                    labels: frame_range,
+                    datasets: [{
+                        label: "Value ",
+                        lineTension: 0.3,
+                        backgroundColor: "rgba(78, 115, 223, 0.05)",
+                        borderColor: "rgba(78, 115, 223, 1)",
+                        pointRadius: 3,
+                        pointBackgroundColor: "rgba(78, 115, 223, 1)",
+                        pointBorderColor: "rgba(78, 115, 223, 1)",
+                        pointHoverRadius: 3,
+                        pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+                        pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+                        pointHitRadius: 10,
+                        pointBorderWidth: 2,
+                        data: y1_axis,
                     }],
-                    yAxes: [{
-                        ticks: {
-                            maxTicksLimit: 5,
-                            padding: 10,
-                            // Include a dollar sign in the ticks
-                            callback: function (value, index, values) {
-                                return number_format(value);
+                },
+                options: {
+                    maintainAspectRatio: false,
+                    layout: {
+                        padding: {
+                            left: 10,
+                            right: 25,
+                            top: 25,
+                            bottom: 0
+                        }
+                    },
+                    scales: {
+                        xAxes: [{
+                            time: {
+                                unit: 'date'
+                            },
+                            gridLines: {
+                                display: false,
+                                drawBorder: false
+                            },
+                            ticks: {
+                                maxTicksLimit: 7
                             }
-                        },
-                        gridLines: {
-                            color: "rgb(234, 236, 244)",
-                            zeroLineColor: "rgb(234, 236, 244)",
-                            drawBorder: false,
-                            borderDash: [2],
-                            zeroLineBorderDash: [2]
-                        }
-                    }],
-                },
-                legend: {
-                    display: false
-                },
-                tooltips: {
-                    backgroundColor: "rgb(255,255,255)",
-                    bodyFontColor: "#858796",
-                    titleMarginBottom: 10,
-                    titleFontColor: '#6e707e',
-                    titleFontSize: 14,
-                    borderColor: '#dddfeb',
-                    borderWidth: 1,
-                    xPadding: 15,
-                    yPadding: 15,
-                    displayColors: false,
-                    intersect: false,
-                    mode: 'index',
-                    caretPadding: 10,
-                    callbacks: {
-                        label: function (tooltipItem, chart) {
-                            var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-                            return datasetLabel + number_format(tooltipItem.yLabel);
+                        }],
+                        yAxes: [{
+                            ticks: {
+                                maxTicksLimit: 5,
+                                padding: 10,
+                                // Include a dollar sign in the ticks
+                                callback: function (value, index, values) {
+                                    return number_format(value);
+                                }
+                            },
+                            gridLines: {
+                                color: "rgb(234, 236, 244)",
+                                zeroLineColor: "rgb(234, 236, 244)",
+                                drawBorder: false,
+                                borderDash: [2],
+                                zeroLineBorderDash: [2]
+                            }
+                        }],
+                    },
+                    legend: {
+                        display: false
+                    },
+                    tooltips: {
+                        backgroundColor: "rgb(255,255,255)",
+                        bodyFontColor: "#858796",
+                        titleMarginBottom: 10,
+                        titleFontColor: '#6e707e',
+                        titleFontSize: 14,
+                        borderColor: '#dddfeb',
+                        borderWidth: 1,
+                        xPadding: 15,
+                        yPadding: 15,
+                        displayColors: false,
+                        intersect: false,
+                        mode: 'index',
+                        caretPadding: 10,
+                        callbacks: {
+                            label: function (tooltipItem, chart) {
+                                var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                                return datasetLabel + number_format(tooltipItem.yLabel);
+                            }
                         }
                     }
                 }
+            });
+        } else {
+            var y2_axis = []
+            for (i = 0; i < data.length; i++) {
+                var curr_data = (data[i])[match_keypoint_column]
+                y2_axis.push(curr_data)
             }
-        });
-
-
+            columns.splice(columns.indexOf(match_keypoint_column),1) // remove it because we don't want to plot this column again.
+            var myLineChart = new Chart(canvas_tag, {
+                type: 'line',
+                data: {
+                    labels: frame_range,
+                    datasets: [{
+                        label: column,
+                        lineTension: 0.3,
+                        backgroundColor: "rgba(78, 115, 223, 0.05)",
+                        borderColor: "rgba(78, 115, 223, 1)",
+                        pointRadius: 3,
+                        pointBackgroundColor: "rgba(78, 115, 223, 1)",
+                        pointBorderColor: "rgba(78, 115, 223, 1)",
+                        pointHoverRadius: 3,
+                        pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+                        pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+                        pointHitRadius: 10,
+                        pointBorderWidth: 2,
+                        data: y1_axis,
+                    },
+                    {
+                        label: match_keypoint_column,
+                        lineTension: 0.3,
+                        backgroundColor: "rgba(78, 115, 223, 0.05)",
+                        borderColor: "rgba(255, 0, 0, 1)",
+                        pointRadius: 3,
+                        pointBackgroundColor: "rgba(78, 115, 223, 1)",
+                        pointBorderColor: "rgba(255, 0, 0, 1)",
+                        pointHoverRadius: 3,
+                        pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+                        pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+                        pointHitRadius: 10,
+                        pointBorderWidth: 2,
+                        data: y2_axis,
+                    }],
+                },
+                options: {
+                    maintainAspectRatio: false,
+                    layout: {
+                        padding: {
+                            left: 10,
+                            right: 25,
+                            top: 25,
+                            bottom: 0
+                        }
+                    },
+                    scales: {
+                        xAxes: [{
+                            time: {
+                                unit: 'date'
+                            },
+                            gridLines: {
+                                display: false,
+                                drawBorder: false
+                            },
+                            ticks: {
+                                maxTicksLimit: 7
+                            }
+                        }],
+                        yAxes: [{
+                            ticks: {
+                                maxTicksLimit: 5,
+                                padding: 10,
+                                // Include a dollar sign in the ticks
+                                callback: function (value, index, values) {
+                                    return number_format(value);
+                                }
+                            },
+                            gridLines: {
+                                color: "rgb(234, 236, 244)",
+                                zeroLineColor: "rgb(234, 236, 244)",
+                                drawBorder: false,
+                                borderDash: [2],
+                                zeroLineBorderDash: [2]
+                            }
+                        }],
+                    },
+                    legend: {
+                        display: true
+                    },
+                    tooltips: {
+                        backgroundColor: "rgb(255,255,255)",
+                        bodyFontColor: "#858796",
+                        titleMarginBottom: 10,
+                        titleFontColor: '#6e707e',
+                        titleFontSize: 14,
+                        borderColor: '#dddfeb',
+                        borderWidth: 1,
+                        xPadding: 15,
+                        yPadding: 15,
+                        displayColors: false,
+                        intersect: false,
+                        mode: 'index',
+                        caretPadding: 10,
+                        callbacks: {
+                            label: function (tooltipItem, chart) {
+                                var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                                return datasetLabel + number_format(tooltipItem.yLabel);
+                            }
+                        }
+                    }
+                }
+            });
+        }
     })
 }
