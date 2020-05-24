2dea6305620c4a9907abd3656d31337db1361d2b
tommarz <tommarz@post.bgu.ac.il>
2020-05-24 22:06:38 +0300

    added shell script for cloning training repos

---
+++ training/clone_training_repos.bat
@@ -0,0 +1,7 @@
+REM Vanilla OpenPose
+git clone https://github.com/tommarz/openpose_train.git
+git clone https://github.com/CMU-Perceptual-Computing-Lab/openpose_caffe_train.git
+REM OpenPose-Plus
+git clone https://github.com/tommarz/openpose-plus.git
+REM Coco Annotator
+git clone https://github.com/jsbroks/coco-annotator.git
