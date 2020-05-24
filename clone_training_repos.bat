be6c8c20eb0c10cd0a997321c97621c0aa1caf06
tommarz <tommarz@post.bgu.ac.il>
2020-05-24 19:32:59 +0300

    1. Updated clone_training_repos.bat script 2. Updated .gitignore 3. Updated json_ops.py module

--- clone_training_repos.bat
+++
@@ -1,9 +0,0 @@
-mkdir training
-cd training
-REM Vanilla OpenPose
-git clone https://github.com/tommarz/openpose_train.git
-git clone https://github.com/CMU-Perceptual-Computing-Lab/openpose_caffe_train.git
-REM OpenPose-Plis
-git clone https://github.com/tommarz/openpose-plus.git
-REM Coco Annotator
-git clone https://github.com/jsbroks/coco-annotator.git
--- training/utils/json_ops.py
+++ training/utils/json_ops.py
@@ -2,12 +2,12 @@ import json
 import shutil
 import os
 
-
 # We assume the openpose training and cooc-annotator repositories
 # are located at the same level with the client, server, utils dirs
 openpose_train_path = "../openpose_train/"
-annotator_path = "../../coco-annotator/"
-coco_skeleton = [[16, 14], [14, 12], [17, 15], [15, 13], [12, 13], [6, 12], [7, 13], [6, 7], [6, 8], [7, 9], [8, 10], [9, 11], [2, 3], [1, 2], [1, 3], [2, 4], [3, 5], [4, 6], [5, 7]]
+annotator_path = "../coco-annotator/"
+coco_skeleton = [[16, 14], [14, 12], [17, 15], [15, 13], [12, 13], [6, 12], [7, 13], [6, 7], [6, 8], [7, 9], [8, 10],
+                 [9, 11], [2, 3], [1, 2], [1, 3], [2, 4], [3, 5], [4, 6], [5, 7]]
 
 '''
 Iterate over all the key value pairs in dictionary and call the given
@@ -34,12 +34,14 @@ def json_field_len(json_path, field_name):
 
 
 def diff(first, second):
-        second = set(second)
-        return [item for item in first if item not in second]
+    second = set(second)
+    return [item for item in first if item not in second]
 
 
-def filter_json(json_path, filters, out_path=None):
-    if out_path is None:
+# Filtering the json attributes given in the filters variable
+# By default, data with no segmentation will be filtered
+def filter_json(json_path, filters, out_path=None, segmentation=True):
+    if out_path is None:  # Overwrite the input json
         out_path = json_path
     with open(json_path, 'r+') as f:
         parsed = json.load(f)
@@ -47,17 +49,38 @@ def filter_json(json_path, filters, out_path=None):
     annots = parsed['annotations']
     filtered_imgs = [filter_dict(img, lambda elem: elem[0] in filters) for img in imgs]
     filtered_annots = [filter_dict(annot, lambda elem: elem[0] in filters) for annot in annots]
+
+    # Removing annotations with no key points and images with no annotations
     annos_to_rem = []
     imgs_to_rem = []
     for anno in filtered_annots:
         try:
-            anno['keypoints']
+            kp = anno['keypoints']
+            if segmentation and anno['area'] is 0:
+                print('removed img with id: ', anno['image_id'])
+                imgs_to_rem.append(anno['image_id'])
+                annos_to_rem.append(anno)
         except Exception as e:
+            print('removed img with id: ', anno['image_id'])
             imgs_to_rem.append(anno['image_id'])
             annos_to_rem.append(anno)
     filtered_imgs = list(filter(lambda img: img['id'] not in imgs_to_rem, filtered_imgs))
-    parsed['images'] = filtered_imgs
+
+    # Updating the json
     parsed['annotations'] = list(filter(lambda annot: annot not in annos_to_rem, filtered_annots))
+    parsed['images'] = list(filter(lambda img: img['id'] in map(lambda anno: anno['image_id'], parsed['annotations']), filtered_imgs))
+
+    # Remapping the images id so it would be 1 to N when N is the number of images
+    new_img_id_map = {img['id']: idx + 1 for (idx, img) in enumerate(filtered_imgs)}
+    for img in parsed['images']:
+        img['id'] = new_img_id_map[img['id']]
+    # Fixing the annotations accordingly
+    for anno in parsed['annotations']:
+        # try:
+        anno['image_id'] = new_img_id_map[anno['image_id']]
+        # except Exception as e:
+        #     print(e)
+
     parsed['categories'][0]['skeleton'] = coco_skeleton
     with open(out_path, 'w') as f:
         json.dump(parsed, f)
@@ -78,6 +101,7 @@ def copy_rename_img(img, dst_path):
     # print(img)
     img_id = img['id']
     img['file_name'] = '%012d' % img_id + ".jpg"
+    # new_filename = '%012d' % idx + ".jpg"
     new_path = dst_path + "/" + img['file_name']
     old_path = annotator_path + img['path']
     img['path'] = new_path
@@ -91,6 +115,9 @@ if __name__ == '__main__':
     openpose_train_annots_path = openpose_train_path + "dataset/COCO/cocoapi/annotations/"
     openpose_train_custom_dataset_path = openpose_train_path + "dataset/COCO/cocoapi/images/custom"
     if not os.path.exists(openpose_train_custom_dataset_path):
+        os.mkdir(openpose_train_custom_dataset_path)
+    else:
+        shutil.rmtree(openpose_train_custom_dataset_path, ignore_errors=False, onerror=None)
         os.mkdir(openpose_train_custom_dataset_path)
 
     filtered_json_path = openpose_train_annots_path + "person_keypoints_custom.json"
