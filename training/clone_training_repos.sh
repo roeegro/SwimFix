b43541692b15e8f3d348df01bca741d8ec10fd3a
tommarz <tommarz@post.bgu.ac.il>
2020-06-07 19:54:21 +0300

    Updated cloning scripts

--- training/clone_training_repos.sh
+++ training/clone_training_repos.sh
@@ -1,6 +1,6 @@
 echo 'Vanilla OpenPose'
 git clone https://github.com/tommarz/openpose_train.git
-git clone https://github.com/CMU-Perceptual-Computing-Lab/openpose_caffe_train.git
+git clone https://github.com/tommarz/openpose_caffe_train.git
 echo 'OpenPose-Plus'
 git clone https://github.com/tommarz/openpose-plus.git
 echo 'Coco Annotator'
